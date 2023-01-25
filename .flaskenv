9627c9c39ec9db39289dfc69ee3240dc800e845d
Marcin Andrzejczak <amostori@op.pl>
2023-01-25 23:06:57 +0100

    favicon

--- .flaskenv
+++ .flaskenv
@@ -1,2 +1,2 @@
 FLASK_APP=app
-FLASK_DEBUG=false
+FLASK_DEBUG=true
--- pages/routes.py
+++ pages/routes.py
@@ -47,6 +47,11 @@ def login():
         email = request.form.get('email')
         password = request.form.get('password')
         my_query = {'email': email}
+        user_data = current_app.db.users.find_one({"email": email})
+        if not user_data:
+            flash('Login credentials not correct')
+            return redirect(url_for('.login'))
+
         hashed_password = current_app.db.users.find_one(my_query)['password']
         if pbkdf2_sha256.verify(password, hashed_password):
             session['email'] = email
@@ -59,36 +64,11 @@ def login():
 @pages.route('/logout', methods=['GET', 'POST'])
 def logout():
     session.clear()
-    email = ''
-    if request.method == 'POST':
-        email = request.form.get('email')
-        password_form = request.form.get('password')
-        myquery = {"email": email}
-        user_hash_password = current_app.db.users.find_one(myquery)['password']
-        if pbkdf2_sha256.verify(password_form, user_hash_password):
-            session['email'] = email
-            flash('Zalogowałeś się skutecznie')
-            return redirect(url_for('page.index'))
-        flash('Nieprawidłowe hasło lub email')
-    return render_template('login.html', email=email)
-
-
-@pages.route('/left-sidebar')
-def left():
-    return render_template('left-sidebar.html')
+    return redirect(url_for('.login'))
 
 
 @pages.route('/zamowienia')
+@login_required
 def zamowienia():
     email = session['email']
     return render_template('zamowienia.html', email=email)
-
-
-@pages.route('/no-sidebar')
-def no_sidebar():
-    return render_template('no-sidebar.html')
-
-
-@pages.route('/elements')
-def elements():
-    return render_template('elements.html')
